85ac078685d418e4ffff0c574c99df17b2e79a3c
Shoaib khan <shoaibmangal@yahoo.com>
2025-03-27 01:33:07 +0500

    add files to js practice 04

---
+++ app.js
@@ -0,0 +1,53 @@
+
+
+// function addition(a,b){
+//     return (10 + 9);
+// }
+// console.log(addition());
+
+// function multiply (a,b){
+//    console.log(30 * 5);
+// }
+// multiply()
+
+
+// function processingData(){
+//    console.log("processing.....")
+// }
+// // function processedData(){
+// //     console.log(processedData)
+// // }
+// processingData()
+
+
+// function greet(){
+//     console.log("hello my name is shoaib")
+// }
+// greet()
+
+
+// for (let index = 0; index <= 100; index++){
+//    console.log(index)
+// }
+
+// for(let name = 0; name <= 100; name++){
+//     console.log(name)
+// }
+
+// function myFirst() {
+//     myDisplayer("Hello");
+//   }
+  
+//   function mySecond() {
+//     myDisplayer("Goodbye");
+//   }
+  
+//   myFirst()
+//   mySecond();
+
+
+  function multiply(number1, number2){
+    console.log(number1, number2);
+  }
+
+  myName()
